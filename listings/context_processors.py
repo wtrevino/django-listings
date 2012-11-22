829834cf13ba5c603063fa20bdeeb06c3984289a
wtrevino <walter.trevino@gmail.com>
2012-11-21 19:09:22 -0600

    Cleaned context processors.

--- listings/context_processors.py
+++ listings/context_processors.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from listings.conf import settings
-from listings.models import Job, Type
+from listings.models import Type
+from categories.models import Category
+
 
 def general_settings(request):
     tv = {}
     tv['LISTINGS_SITE_NAME'] = settings.LISTINGS_SITE_NAME
-    tv['LISTINGS_HTML_TITLE'] = settings.LISTINGS_HTML_TITLE 
+    tv['LISTINGS_HTML_TITLE'] = settings.LISTINGS_HTML_TITLE
     tv['LISTINGS_SITE_KEYWORDS'] = settings.LISTINGS_SITE_KEYWORDS
     tv['LISTINGS_SITE_DESCRIPTION'] = settings.LISTINGS_SITE_DESCRIPTION
     return tv
 
+
 def categories_and_types(request):
     tv = {}
-    tv['listings_categories'] = Category.on_site.all().order_by('category_order')
+    tv['listings_categories'] = Category.on_site.all().order_by('order')
     tv['listings_types'] = Type.on_site.all()
-    
+    return tv
