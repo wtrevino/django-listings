93b0cd835b0653b0fcfbe1d1827127e255b3ba35
wtrevino <walter.trevino@gmail.com>
2013-01-21 17:33:03 -0600

    Only display template code in FeedTypeForm when value is a FileField.

--- listings/syndication/admin.py
+++ listings/syndication/admin.py
@@ -12,7 +12,8 @@ class FeedTemplateInput(ClearableFileInput):
     def render(self, name, value, attrs=None):
         def _get_template_content(value):
             return '<div style="border: 1px solid #bbb; border-radius: 10px; padding: 5px;"><code> %s </code></div>' % value.read().replace(' ', '&nbsp;').replace('<', '&lt;').replace('>', '&gt;').replace('\n', '<br />')  # i know... ugly :D
-        self.template_with_initial = u'%(initial_text)s: %(initial)s %(clear_template)s <br /><br /> ' + _get_template_content(value) + ' <br />%(input_text)s: %(input)s'
+        if hasattr(value, 'read'):
+            self.template_with_initial = u'%(initial_text)s: %(initial)s %(clear_template)s <br /><br /> ' + _get_template_content(value) + ' <br />%(input_text)s: %(input)s'
         return super(FeedTemplateInput, self).render(name, value, attrs=None)
 
 
