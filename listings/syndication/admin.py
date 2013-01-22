78c985cd13407b7af96e9d28b06a680addeb1aa1
wtrevino <walter.trevino@gmail.com>
2013-01-21 18:16:38 -0600

    Replace % character in FeedType.template rendering in admin.

--- listings/syndication/admin.py
+++ listings/syndication/admin.py
@@ -11,9 +11,9 @@ class FeedTemplateInput(ClearableFileInput):
 
     def render(self, name, value, attrs=None):
         def _get_template_content(value):
-            return '<div style="border: 1px solid #bbb; border-radius: 10px; padding: 5px;"><code> %s </code></div>' % value.read().replace(' ', '&nbsp;').replace('<', '&lt;').replace('>', '&gt;').replace('\n', '<br />')  # i know... ugly :D
+            return '<div style="border: 1px solid #bbb; border-radius: 10px; padding: 5px;"><code> %s </code></div>' % value.read().replace(' ', '&nbsp;').replace('<', '&lt;').replace('>', '&gt;').replace('\n', '<br />').replace('%', '&#37;')  # i know... ugly :D
         if hasattr(value, 'read'):
-            self.template_with_initial = u'%(initial_text)s: %(initial)s %(clear_template)s <br /><br /> ' + _get_template_content(value) + ' <br />%(input_text)s: %(input)s'
+            self.template_with_initial = u'%(initial_text)s: %(initial)s %(clear_template)s<br /><br /> ' + _get_template_content(value) + '   %(input_text)s: %(input)s'
         return super(FeedTemplateInput, self).render(name, value, attrs=None)
 
 
