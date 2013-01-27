9f1c4a31d779ba2de0210bb64771635570c373a1
wtrevino <walter.trevino@gmail.com>
2013-01-27 11:25:36 -0600

    display_feed view now displays only active, per site ads.

--- listings/syndication/views.py
+++ listings/syndication/views.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import Context
 
+from django.contrib.sites.models import Site
 from listings.syndication.models import Feed
-from listings.models import Job
 
 
 def display_feed(request, feed_url):
-    feed = get_object_or_404(Feed, feed_url=feed_url)
-    ads = Job.active.all()
+    site = Site.objects.get_current()
+    try:
+        feed = site.feed_set.get(feed_url=feed_url)
+    except Feed.DoesNotExist:
+        raise Http404
     template = feed.get_template()
-    context = Context({'ads': ads})
+    context = Context({'ads': feed.ads.model.active.all()})
     return HttpResponse(template.render(context), content_type=feed.content_type)
