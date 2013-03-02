c110cb3e4f78e27f48a9ee6ab8a397383b245c9a
wtrevino <walter.trevino@gmail.com>
2013-03-01 23:52:13 -0600

    Improved email obfuscation filter, replace anchor with anchor text.

--- listings/templatetags/obfuscate.py
+++ listings/templatetags/obfuscate.py
@@ -5,6 +5,8 @@ from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 from django.utils.html import strip_tags
 
+from bs4 import BeautifulSoup
+
 import re
 
 from rendertext import render
@@ -22,8 +24,16 @@ def get_strings(string):
 @register.filter()
 @stringfilter
 def obfuscate(value):
-    value = strip_tags(value)
-    word_list = value.split()
+
+    # remove all anchors
+    soup = BeautifulSoup(value, 'html.parser')
+    for anchor in soup.findAll('a'):
+        anchor.replaceWith(anchor.string)
+    value = soup.prettify()
+
+    # find all email strings
+    _value = strip_tags(value)
+    word_list = _value.split()
     email_strings = []
     for word in word_list:
         found_strings = get_strings(word)
@@ -31,8 +41,9 @@ def obfuscate(value):
             if found_string not in email_strings:
                 email_strings.append(found_string)
 
+    # replace email strings with its generated image
     for string in email_strings:
         rendered = render(string, 'verdana')
-        value = value.replace(string, '<img src="/%s" width="%s" height="%s" title="Email protection" alt="Email protection" style="position: relative; top: 2px;" />' % (rendered[0], rendered[1], rendered[2]))
+        value = value.replace(string, '<img src="/%s" width="%s" height="%s" style="position: relative; top: 2px;" />' % (rendered[0], rendered[1], rendered[2]))
 
     return mark_safe(value)
